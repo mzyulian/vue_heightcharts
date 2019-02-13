142d6df1fa0c804bc589aa025a5db296a0f398a6
mzyulian <y13726014889@163.com>
2019-02-13 17:29:10 +0800

    '柱状图表'

---
+++ src/views/column/index.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- heightcharts 柱状图表 -->
+    <div class='page_column'>
+
+    </div>
+</template>
+
+<script>
+import column from '@/components/column'
+export default {
+  components: {
+    column
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
