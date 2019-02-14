79ca21e15333e2436f23eb5b1ce730d810864d05
mzyulian <y13726014889@163.com>
2019-02-14 09:11:15 +0800

    柱状图

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
 import Heightpie from '@/views/heightpie'
+import Column from '@/views/column'
 
 Vue.use(Router)
 
@@ -13,9 +14,16 @@ export default new Router({
       component: Home
     },
     {
+      /* 饼状图 */
       path: '/heightpie',
       name: 'heightpie',
       component: Heightpie
+    },
+    {
+      /* 柱状图 */
+      path: '/column',
+      name: 'column',
+      component: Column
     }
   ]
 })
